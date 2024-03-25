5de777e21a95a899f13b02a692eb62ab287a9dea
xiro_codes <github@tdavis.dev>
2024-03-25 16:10:44 -0500

    the rest

--- packages/eww-scripts/default.nix
+++ packages/eww-scripts/default.nix
@@ -15,9 +15,21 @@
 			name = "weather_info";
 			text = builtins.readFile ./weather_info.sh;
 		};
+	quotes = writeShellApplication {
+			name = "quotes";
+			text = builtins.readFile ./quotes.sh;
+		};
+	sys_info = writeShellApplication {
+			name = "sys_info";
+			text = builtins.readFile ./sys_info.sh;
+		};
+	system = writeShellApplication {
+			name = "system";
+			text = builtins.readFile ./system.sh;
+		};
 in
   symlinkJoin {
     name = "eww-scripts";
-    paths = [music_info weather_info];
+    paths = [music_info weather_info quotes sys_info system];
     meta.mainPrograms = "";
   }
