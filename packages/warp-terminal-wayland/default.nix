7d357d0135bc7f458e0c3ba7559655005c0aeb03
xiro_codes <github@tdavis.dev>
2024-03-29 10:18:39 -0500

    update: wrap-terminal

--- packages/warp-terminal-wayland/default.nix
+++ packages/warp-terminal-wayland/default.nix
@@ -1,19 +1,19 @@
-{
-  lib,
-  inputs,
-  pkgs,
-  stdenv,
-  ...
+{ lib
+, inputs
+, pkgs
+, stdenv
+, ...
 }:
 pkgs.warp-terminal.overrideAttrs (old: rec {
   pname = "warp-terminal-wayland";
-  version = "0.2024.03.19.08.01.stable_01";
-  src = pkgs.fetchurl {
-    url = "https://releases.warp.dev/stable/v${version}/warp-terminal-v${version}-1-x86_64.pkg.tar.zst";
-    sha256 = "sha256-efnYh48xcLneeotH9iSY0xQRgMXI/erM6F2fIH38yjY=";
-  };
-  nativeBuildInputs = old.nativeBuildInputs ++ [pkgs.makeWrapper stdenv];
-  buildInputs = old.buildInputs ++ [pkgs.wayland pkgs.wayland-protocols];
+  version = "0.2024.03.26.08.02.stable_02";
+  src = pkgs.fetchurl
+    {
+      url = "https://releases.warp.dev/stable/v${version}/warp-terminal-v${version}-1-x86_64.pkg.tar.zst";
+      sha256 = "sha256-hBwLfxhdy4cru2xH3hY+mGbJpR47Qf3SqEIijIRDstU=";
+    };
+  nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.makeWrapper stdenv ];
+  buildInputs = old.buildInputs ++ [ pkgs.wayland pkgs.wayland-protocols ];
   postInstall = ''
     makeWrapper $out/bin/warp-terminal $out/bin/warp-terminal-wayland --set WARP_ENABLE_WAYLAND 1 \
     	--prefix LD_LIBRARY_PATH : ${pkgs.wayland}/lib
