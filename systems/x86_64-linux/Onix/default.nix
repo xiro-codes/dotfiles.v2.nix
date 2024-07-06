f745666d018c3228d6f4243c906450b546beede7
xiro_codes <github@tdavis.dev>
2024-07-06 11:24:12 -0500

    add: add gitea

--- systems/x86_64-linux/Onix/default.nix
+++ systems/x86_64-linux/Onix/default.nix
@@ -68,12 +68,16 @@
       user = "tod";
       group = "users";
     };
+    gitea = {
+      enable = true;
+      stateDir = "/mnt/storage/gitea";
+    };
   };
   fileSystems."/mnt/storage" = {
     device = "/dev/disk/by-id/ata-WDC_WD10EZEX-22MFCA0_WD-WCC6Y2AV1NZC-part1";
   };
-	fileSystems."/mnt/media" = {
-		device = "/dev/disk/by-id/ata-ST8000DM004-2U9188_ZR12S72H-part1";
-	};
+  fileSystems."/mnt/media" = {
+    device = "/dev/disk/by-id/ata-ST8000DM004-2U9188_ZR12S72H-part1";
+  };
   system.stateVersion = "24.05";
 }
